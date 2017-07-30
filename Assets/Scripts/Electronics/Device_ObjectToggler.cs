cbf851d74d28ca3c42a04eb99003f36e90d89a6e
Ivan Notaros <nothke@gmail.com>
2017-07-30 04:54:04 +0200

    Added object toggler device for testing and scene

---
+++ Assets/Scripts/Electronics/Device_ObjectToggler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device_ObjectToggler : ElectronicComponent
+{
+    public GameObject enableOnPowered;
+    public GameObject enableOnShutdown;
+
+    public override void OnPowered()
+    {
+        if (enableOnPowered)
+            enableOnPowered.SetActive(true);
+
+        if (enableOnShutdown)
+            enableOnShutdown.SetActive(false);
+    }
+
+    public override void OnShutDown()
+    {
+        if (enableOnPowered)
+            enableOnPowered.SetActive(false);
+
+        if (enableOnShutdown)
+            enableOnShutdown.SetActive(true);
+    }
+}

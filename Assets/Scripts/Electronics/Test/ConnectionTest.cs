a1b3d7e30d91b1b4fde6b397c4ef5d833dd2af61
Ivan Notaros <nothke@gmail.com>
2017-07-30 04:28:46 +0200

    Added test and benchmark

---
+++ Assets/Scripts/Electronics/Test/ConnectionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionTest : ElectronicComponent
+{
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = powered ? Color.green : Color.gray;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+
+        foreach (var c in connected)
+        {
+            Gizmos.DrawLine(c.transform.position, transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Electronics/Test/ElectronicComponentBenchmark.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronicComponentBenchmark : MonoBehaviour
+{
+    public int numOfNodes = 100;
+    public int maxBranchNodes = 10;
+    public float chance = 0.1f;
+
+    void Start()
+    {
+        ElectronicComponent prevEC = null;
+
+        for (int i = 0; i < numOfNodes; i++)
+        {
+            GameObject go = new GameObject("Test");
+            go.transform.position = new Vector3(i * 2, 0, 0);
+
+            var connectionTest = go.AddComponent<ConnectionTest>();
+
+            if (Random.value < chance)
+            {
+                ElectronicComponent prevECVert = connectionTest;
+
+                int size = Random.Range(1, maxBranchNodes);
+                for (int j = 0; j < size; j++)
+                {
+                    GameObject goVert = new GameObject("Test");
+                    goVert.transform.position = new Vector3(i * 2, (j + 1) * 2, 0);
+
+                    var cTVert = goVert.AddComponent<ConnectionTest>();
+
+                    cTVert.ConnectTo(prevECVert);
+
+                    if (Random.value < 0.05f)
+                        goVert.AddComponent<PowerSource>();
+
+                    prevECVert = cTVert;
+                }
+            }
+
+            if (i != 0)
+                connectionTest.ConnectTo(prevEC);
+
+            prevEC = connectionTest;
+        }
+
+
+        prevEC.gameObject.AddComponent<PowerSource>();
+
+        prevEC.UpdatePowerState();
+    }
+}
